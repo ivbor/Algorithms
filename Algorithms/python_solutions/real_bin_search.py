3ba7f97ef484cea20aaf74a4e6bd24d8e63cb01d
ivbor <borovoy.vanya@gmail.com>
2023-08-10 17:01:30 +0300

    added binary searches and coverage for them

--- Algorithms/python_solutions/real_bin_search.py
+++ Algorithms/python_solutions/real_bin_search.py
@@ -1,6 +1,5 @@
-import math
-
-def real_bin_search(func, y, l, r, eps = 1e-6):
+def real_bin_search(func, func_value, left_edge,
+                    right_edge, eps=1e-6, check=False):
     '''
         basic binary search among real
         numbers for x where func(x) = y
@@ -9,12 +8,26 @@ def real_bin_search(func, y, l, r, eps = 1e-6):
         many eps will fit in abs(r-l)
         r - right edge of search interval
         l - left one accordingly
+
+        by default assumes required x
+        to be between left and right edges
+        but can check that
+        if check is set to True
     '''
 
     # this algo requires O(log2(n))
     # operations where n is the amount of
     # the smallest cuts we consider (epsilons)
     # that can fit in abs(r-l)
+
+    if check:
+        result = real_bin_search(
+            func,
+            func_value,
+            left_edge - 1,
+            right_edge + 1)
+        if (result < left_edge or right_edge < result):
+            raise KeyError('func_value is unreachable within given edges')
 
     # determine whether function is
     # ascending or descending
@@ -23,16 +36,16 @@ def real_bin_search(func, y, l, r, eps = 1e-6):
     # so that search can work
     # even if function does not
     # exist in l or r
-    asc = (func(l+eps) < func(r-eps))
+    is_ascending = (func(left_edge + eps) < func(right_edge - eps))
 
     # we can use while to ensure accuracy
     # is no less than eps
-    while (abs(r-l) >= eps):
+    while (abs(right_edge - left_edge) >= eps):
 
         # find the middle of the cut
-        m = (l + r)/2
+        middle = (left_edge + right_edge)/2
 
-        if asc:
+        if is_ascending:
 
             # if function is ascending
             # and function is less than y
@@ -40,8 +53,8 @@ def real_bin_search(func, y, l, r, eps = 1e-6):
             # in bigger x's (righer)
             # so move left border of the
             # current cut to the middle
-            if func(m) < y:
-                l = m
+            if func(middle) < func_value:
+                left_edge = middle
 
             # if ascending and function
             # is more than y - this means
@@ -49,18 +62,18 @@ def real_bin_search(func, y, l, r, eps = 1e-6):
             # so move right border of the
             # current cut to the middle
             else:
-                r = m
+                right_edge = middle
 
         # for descending - everything
         # works accordingly with the
         # change of sign in if clause
         else:
-            if func(m) > y:
-                l = m
+            if func(middle) > func_value:
+                left_edge = middle
             else:
-                r = m
+                right_edge = middle
 
     # for even more accuracy
     # as an answer give the middle
     # of the accurate enough cut
-    return (l + r)/2
+    return (left_edge + right_edge)/2
---
+++ Algorithms/tests/test_bin_search.py
@@ -0,0 +1,17 @@
+import random
+
+from Algorithms.python_solutions.bin_search import bin_search
+
+def test_bin_search_no_rec():
+    array = [random.randint(-100, 100) for i in range(random.randint(10, 100))]
+    array = sorted(array)
+    for i in range(-100, 100):
+        assert bin_search(array, i) == (i in array), \
+            f'existence of {i} inside array is determined wrong'
+
+def test_bin_search_with_rec():
+    array = [random.randint(-100, 100) for i in range(random.randint(10, 100))]
+    array = sorted(array)
+    for i in range(-100, 100):
+        assert bin_search(array, i, True) == (i in array), \
+            f'existence of {i} inside array is determined wrong'
