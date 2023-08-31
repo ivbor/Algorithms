8408a98aea6be29321f99b43ae035f2817b6398e
ivbor <borovoy.vanya@gmail.com>
2023-08-31 07:31:28 +0400

    fixed pep8 blank lines

--- Algorithms/python_solutions/split_find.py
+++ Algorithms/python_solutions/split_find.py
@@ -1,20 +1,22 @@
 import random
 
+
 def split(a, x, l, r):
     m = l
     for i in range(l, r):
         if a[i] < x:
             a[i], a[m] = a[m], a[i]
-            m+=1
+            m += 1
     new_l = m
     if m == len(a):
-        return 0,0
+        return 0, 0
     for i in range(new_l, r):
-        if abs(a[i] - x) <= 10**-14 :
+        if abs(a[i] - x) <= 10**-14:
             a[i], a[m] = a[m], a[i]
-            m+=1
+            m += 1
     new_r = m
     return new_l, new_r
+
 
 def _split_find(a, l, r, k):
     x = a[random.randint(l, r-1)]
@@ -25,6 +27,7 @@ def _split_find(a, l, r, k):
         return _split_find(a, l, m, k)
     else:
         return _split_find(a, m, r, k)
+
 
 def split_find(a, *args):
     """
