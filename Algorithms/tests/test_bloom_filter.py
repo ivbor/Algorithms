0e02cdae944f158229aff35fd8fcd85984e28d1a
ivbor <46250545+ivbor@users.noreply.github.com>
2023-09-22 10:23:37 +0200

    Update test_bloom_filter.py
    Added assertion test for bloom filter's false positives

--- Algorithms/tests/test_bloom_filter.py
+++ Algorithms/tests/test_bloom_filter.py
@@ -31,6 +31,15 @@ def test_can_create_bloom_filter_jenkins():
             fails_count += 1
     assert fails_count < 60, 'fails_count does not converge to 50'
 
+def test_false_positives_less_than_5_percent(bf):
+    fails_count = 0
+    for i in range(10000):
+        bf.add(i)
+    for i in range(10000, 20000):
+        if bf.check(i):
+            fails_count += 1
+    assert fails_count >= 500, 
+        'fails exceed required range' 
 
 # make a distinct file with this test
 def test_bloom_filter_stress():
