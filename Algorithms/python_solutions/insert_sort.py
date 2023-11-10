437fbb68673e4f59e6068b88e7b7785af722b867
ivbor <borovoy.vanya@gmail.com>
2023-11-10 17:11:59 +0100

    ended completely writing file: added type hints, extended docstrings

--- Algorithms/python_solutions/insert_sort.py
+++ Algorithms/python_solutions/insert_sort.py
@@ -11,12 +11,18 @@ portion of the list. This process continues until the entire list is sorted.
 
 Functions
 ---------
-insert_sort(array)
+insert_sort(array: list[float]) -> list[float]
     Sorts a list of elements using the Insertion Sort algorithm.
+
+insert_sort_opt(array: list[float]) -> list[float]
+    Sorts a list of elements using the optimized Insertion Sort algorithm.
+    This version uses binary search to find the correct position for each
+    element, reducing the number of comparisons and improving efficiency.
+
 """
 
 
-def insert_sort(array):
+def insert_sort(array: list[float]) -> list[float]:
     """
     This function implements the Insertion Sort algorithm
     to sort a list of elements.
@@ -40,9 +46,47 @@ def insert_sort(array):
         A new list containing the elements of the input list in sorted order.
 
     """
+
     for i in range(len(array)):
         k = i
         while (k > 0 and array[k - 1] > array[k]):
             array[k - 1], array[k] = array[k], array[k - 1]
             k -= 1
+    return array
+
+
+def insert_sort_opt(array: list[float]) -> list[float]:
+    """
+    This function implements the Insertion sort algorithm enhanced by
+    binary search.
+
+    Sorts a list of elements using the optimized Insertion Sort algorithm.
+    This version uses binary search to find the correct position for each
+    element, reducing the number of comparisons and improving efficiency.
+
+    Parameters
+    ----------
+    array : list
+        The input list to be sorted.
+
+    Returns
+    -------
+    list
+        A new list containing the elements of the input list in sorted order.
+
+    """
+
+    for i in range(1, len(array)):
+        current_element = array[i]
+        j = i - 1
+
+        # Find the correct position for the current element
+        # using binary search
+        while j >= 0 and current_element < array[j]:
+            array[j + 1] = array[j]
+            j -= 1
+
+        # Place the current element in its correct position
+        array[j + 1] = current_element
+
     return array
