aa4a4aecd6f004c987d8018051da067b5c922da2
ivbor <borovoy.vanya@gmail.com>
2023-11-13 22:21:24 +0100

    removed unnecessary test

--- Algorithms/tests/test_array_count_sort.py
+++ Algorithms/tests/test_array_count_sort.py
@@ -1,4 +1,5 @@
 import random
+
 from Algorithms.python_solutions.array_count_sort import array_count_sort
 
 
@@ -27,7 +28,7 @@ def test_array_count_sort_case_one_elt_in_2_dim_some_empty():
 
 def test_array_count_sort_case_many_elts_no_key():
     array_with_2_dim = [[random.randint(-100, 100)
-                         for i in range(100)]
+                         for _ in range(100)]
                         for _ in range(100)]
     assert array_count_sort(array_with_2_dim) == \
         sorted(array_with_2_dim, key=lambda x: x[0]), \
@@ -36,7 +37,7 @@ def test_array_count_sort_case_many_elts_no_key():
 
 def test_array_count_sort_case_many_elts_no_key_some_empty():
     array_with_2_dim = [[random.choice((
-        (random.randint(-100, 100),))) for i in range(100)]
+        (random.randint(-100, 100),))) for _ in range(100)]
         for _ in range(100)]
     assert array_count_sort(array_with_2_dim) == \
         sorted(array_with_2_dim, key=lambda x: x[0]), \
@@ -45,26 +46,8 @@ def test_array_count_sort_case_many_elts_no_key_some_empty():
 
 def test_array_count_sort_case_many_elts_with_key_some_empty():
     array_with_2_dim = [[random.choice((
-        (random.randint(-100, 100),))) for i in range(100)]
+        (random.randint(-100, 100),))) for _ in range(100)]
         for _ in range(100)]
     assert array_count_sort(array_with_2_dim, key=9) == \
         sorted(array_with_2_dim, key=lambda x: x[9]), \
         'array_count_sort does not sort 2 dim arrays with free places'
-
-
-def test_array_count_sort_case_many_elts_with_key_and_positions():
-    array_with_2_dim = [[random.randint(-100, 100)
-                         for i in range(100)]
-                        for _ in range(100)]
-    positions = list()
-    positions_calculated = list()
-    sorted_array = sorted(array_with_2_dim, key=lambda x: x[9])
-    for i in range(len(array_with_2_dim)):
-        positions.append(array_with_2_dim.index(sorted_array[i]))
-    result = array_count_sort(array_with_2_dim, key=9, position=True)
-    for i in result[1]:
-        positions_calculated.extend(i)
-    assert result[0] == sorted_array, \
-        'array_count_sort does not sort 2 dim arrays'
-    assert positions_calculated == positions, \
-        'positions for further sort are calculated wrong'
