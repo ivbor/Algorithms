5e5400f948591ce04a56025732a41035c650e62a
ivbor <borovoy.vanya@gmail.com>
2024-02-22 19:39:15 +0100

    added docstrings to rbtree module

--- Algorithms/python_solutions/red_black_tree.py
+++ Algorithms/python_solutions/red_black_tree.py
@@ -1,22 +1,135 @@
+"""
+Red-Black Tree Module
+=========================
+
+This module defines a Red-Black Tree class which represents balanced
+binary search tree. By coloring nodes it is possible to maintain
+the height of O(logn), here in particular it is 3*log2n.
+
+Classes
+-------
+TreeNode
+    A class representing a node in the Red-Black Tree.
+
+BinarySearchTree
+    A class representing a Red-Black Tree.
+
+"""
+
 import logging
 
 from Algorithms.python_solutions.bst import TreeNode, BinarySearchTree
 
 
 class RBTreeNode(TreeNode):
+    """
+    Red-Black Tree Node Class
+
+    This class represents a node in a red-black tree.
+    It extends the TreeNode class.
+
+    Attributes
+    ----------
+    color: str
+        The color of the node, either 'red' or 'black'.
+
+    """
     def __init__(self, data, color):
+        """
+        Initialize a Red-Black Tree Node.
+
+        Parameters
+        ----------
+        data: any
+            The data to be stored in the node.
+
+        color: str
+            The color of the node, either 'red' or 'black'.
+
+        Returns
+        -------
+        None
+
+        """
         super().__init__(data)
         self.color = color
 
 
 class RedBlackTree(BinarySearchTree):
+    """
+    Red-Black Tree Class
 
-    def __init__(self):
+    This class represents a red-black tree,
+    which is a self-balancing binary search tree.
+
+    Attributes
+    ----------
+    node_class: RBTreeNode
+        The class to use for creating nodes in the tree.
+
+    Methods
+    -------
+    __init__(self) -> None
+        Initialize a RedBlackTree object.
+
+    insert(self, key: int | float) -> None
+        Insert key into RedBlackTree.
+
+    handle_rb(self, new_node: RBTreeNode) -> None
+        The function managing insertion into RedBlackTree.
+
+    recolor(self, node: RBTreeNode, uncle: RBTreeNode) -> None
+        The function handling the case when only recoloring is needed.
+
+    rotate_and_recolor(self, node: RBTreeNode, right: bool) -> None
+        The function handling the case when both rotation and recoloring
+        have to be done.
+
+    _left_rotate(self, node: RBTreeNode) -> None
+        The function performing left rotation with node being moved to its
+        left child place and the right child of the node moved to the
+        node's place.
+
+    _right_rotate(self, node: RBTreeNode) -> None
+        The function performing right rotation with node being moved to its
+        right child place and the left child of the node moved to the
+        node's place.
+
+    _delete(self, node: RBTreeNode) -> None
+        The function performing deletion of the node.
+
+    _fix_double_black(self, node: RBTreeNode) -> None
+        The function handling the double black violation case
+        (parent and child are black) when deleting node.
+
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize a Red-Black Tree.
+
+        Returns
+        -------
+        None
+
+        """
         super().__init__()
         self.node_class = RBTreeNode
 
-    def insert(self, key):
+    def insert(self, key: int | float) -> None:
+        """
+        Insert a key into the red-black tree.
 
+        Parameters
+        ----------
+        key: int | float
+            The key to insert into the tree.
+
+        Returns
+        -------
+        None
+
+        """
         new_node = self.node_class(data=key, color='red')
         self.size += 1
 
@@ -30,7 +143,20 @@ class RedBlackTree(BinarySearchTree):
         self._insert(self.root, new_node)
         self.handle_rb(new_node)
 
-    def handle_rb(self, new_node):
+    def handle_rb(self, new_node: RBTreeNode) -> None:
+        """
+        Handle red-black tree properties after inserting a node.
+
+        Parameters
+        ----------
+        new_node: RBTreeNode
+            The newly inserted node.
+
+        Returns
+        -------
+        None
+
+        """
 
         # if there is root - check the parent color
         # if black - do nothing
@@ -153,7 +279,24 @@ class RedBlackTree(BinarySearchTree):
             else:
                 self.rotate_and_recolor(new_node, right_uncle)
 
-    def recolor(self, node, uncle):
+    def recolor(self, node: RBTreeNode, uncle: RBTreeNode) -> None:
+        """
+        Recolor nodes in the red-black tree after insertion.
+
+        Parameters
+        ----------
+        node: RBTreeNode
+            The newly inserted node.
+
+        uncle: RBTreeNode
+            The uncle node of the newly inserted node.
+
+        Returns
+        -------
+        None
+
+        """
+
         logging.debug('recoloring')
         node.parent.color = 'black'
         logging.debug(self.root.color)
@@ -164,7 +307,24 @@ class RedBlackTree(BinarySearchTree):
             logging.debug(self.root.color)
             self.handle_rb(node.parent.parent)
 
-    def rotate_and_recolor(self, node, right):
+    def rotate_and_recolor(self, node: RBTreeNode, right: bool) -> None:
+        """
+        Rotate and recolor nodes in the red-black tree after insertion.
+
+        Parameters
+        ----------
+        node: RBTreeNode
+            The newly inserted node.
+
+        right: bool
+            Flag indicating if the uncle of the newly inserted node is on
+            the right (in this case is True) or on the left (False).
+
+        Returns
+        -------
+        None
+
+        """
 
         logging.debug('recoloring and rotating')
         # grandparent to red
@@ -234,7 +394,20 @@ class RedBlackTree(BinarySearchTree):
             if node.parent.parent is not None:
                 self._left_rotate(node.parent)
 
-    def _left_rotate(self, node):
+    def _left_rotate(self, node: RBTreeNode) -> None:
+        """
+        Perform a left rotation on the red-black tree.
+
+        Parameters
+        ----------
+        node: RBTreeNode
+            The node around which the rotation is performed.
+
+        Returns
+        -------
+        None
+
+        """
 
         # around the node - right_child0 bond
         #
@@ -267,7 +440,20 @@ class RedBlackTree(BinarySearchTree):
         right_child0.children[0] = node
         node.parent = right_child0
 
-    def _right_rotate(self, node):
+    def _right_rotate(self, node: RBTreeNode) -> None:
+        """
+        Perform a right rotation on the red-black tree.
+
+        Parameters
+        ----------
+        node: RBTreeNode
+            The node around which the rotation is performed.
+
+        Returns
+        -------
+        None
+
+        """
 
         # around the node - left_child0 bond
         #
@@ -300,14 +486,29 @@ class RedBlackTree(BinarySearchTree):
         left_child0.children[1] = node
         node.parent = left_child0
 
-    def _delete(self, node):
+    def _delete(self, node: RBTreeNode) -> None:
+        """
+        Delete a node from the red-black tree.
 
+        Parameters
+        ----------
+        node: RBTreeNode
+            The node to be deleted.
+
+        Returns
+        -------
+        None
+
+        """
+
+        # simple case, both non-Nones
         if node.children[0] is not None and node.children[1] is not None:
             successor = node.children[1]
             while successor.children[0] is not None:
                 successor = successor.children[0]
             node.data, successor.data = successor.data, node.data
             self._delete(successor)
+        # only one is None
         elif node.children[0] is not None or node.children[1] is not None:
             child = node.children[0] if node.children[0] is not None \
                 else node.children[1]
@@ -323,6 +524,7 @@ class RedBlackTree(BinarySearchTree):
                     child.color = 'black'
                 else:
                     self._fix_double_black(child)
+        # both Nones
         else:
             if node.parent is not None:
                 node.parent.color = 'black'
@@ -333,7 +535,21 @@ class RedBlackTree(BinarySearchTree):
             else:
                 self.root.data = None
 
-    def _fix_double_black(self, node: TreeNode):
+    def _fix_double_black(self, node: RBTreeNode) -> None:
+        """
+        Fix double black violation in the red-black tree.
+
+        Parameters
+        ----------
+        node: TreeNode
+            The node causing the double black violation.
+
+        Returns
+        -------
+        None
+
+        """
+
         while node != self.root and node.color == 'black':
             if node is node.parent.children[0] \
                     and node.parent.children[1] is not None:
